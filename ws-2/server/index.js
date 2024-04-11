cb9644212b0579c4401dc9957aac05c3f862156c
vikalp-tomer <vikalptomar15@gmail.com>
2024-04-11 13:32:06 +0530

    video 2 completed

---
+++ ws-2/server/index.js
@@ -0,0 +1,25 @@
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} con`);
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", `${socket.id.substring(0, 5)}${data}`);
+  });
+});
+
+httpServer.listen(3500, () => {
+  console.log("Listening on http://localhost:3500");
+});
